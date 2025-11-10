de8c01ee04fba6f58f175484c0349be6e9a64289
rahebgames <penguin@DESKTOP-Q4N9EV5.localdomain>
2025-11-09 19:04:47 -0800

    put a basic leaflet map on the screen

--- PLAN.md
+++ PLAN.md
@@ -29,7 +29,7 @@ Key gameplay challenge: Can players collect and craft tokens from nearby locatio
 
 ##### Map Basics
 
-- [ ] put a basic leaflet map on the screen
+- [x] put a basic leaflet map on the screen
 - [ ] draw the player's location on the map
 - [ ] draw a rectangle representing one cell on the map
 - [ ] use loops to draw a whole grid of cells on the map
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,44 @@
 // @deno-types="npm:@types/leaflet"
 import Leaflet from "leaflet";
 
-// Style sheets
-import "leaflet/dist/leaflet.css"; // supporting style for Leaflet
-import "./style.css"; // student-controlled page style
-
-// Fix missing marker images
-import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
-
-// Import our luck function
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+import "./_leafletWorkaround.ts";
 import _luck from "./_luck.ts";
 
-// Create basic UI elements
-
-// Our classroom location
-const _CLASSROOM_LATLNG = Leaflet.latLng(
+/* constants */
+const CLASSROOM_LATLNG = Leaflet.latLng(
   36.997936938057016,
   -122.05703507501151,
 );
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const _TILE_DEGREES = 1e-4;
+const _NEIGHBORHOOD_SIZE = 8;
+const _CACHE_SPAWN_PROBABILITY = 0.1;
+
+/* global variables */
+let mapDiv: HTMLDivElement;
+let map: Leaflet.Map;
+
+function createMap(): void {
+  mapDiv = document.createElement("div");
+  mapDiv.id = "map";
+  document.body.append(mapDiv);
+
+  map = Leaflet.map(mapDiv, {
+    center: CLASSROOM_LATLNG,
+    zoom: GAMEPLAY_ZOOM_LEVEL,
+    minZoom: GAMEPLAY_ZOOM_LEVEL,
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    zoomControl: false,
+    scrollWheelZoom: false,
+  });
+
+  Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+}
+
+createMap();
